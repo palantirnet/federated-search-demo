476d32848137aa3e12d532dbcfea1cc6ceb9d707
Malak Desai <mdesai13@hawk.iit.edu>
2018-10-31 10:29:08 -0500

    add drush aliases

---
+++ drush/fsd-d7.aliases.drushrc.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!isset($drush_major_version)) {
+  $drush_version_components = explode('.', DRUSH_VERSION);
+  $drush_major_version = $drush_version_components[0];
+}
+
+$aliases['local'] = array(
+  'root' => '/var/www/federated-search-demo.local/web/d7/web',
+  'uri' => 'www.federated-search-demo.d7.local',
+);
