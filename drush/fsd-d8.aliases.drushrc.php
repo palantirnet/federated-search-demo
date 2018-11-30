9e051538609bcfbbdfa665d49d46a08a6bf02b26
Malak Desai <mdesai13@hawk.iit.edu>
2018-11-30 16:01:47 -0600

    add drush alias and enable domain config

--- drush/fsd-d8.aliases.drushrc.php
+++ drush/fsd-d8.aliases.drushrc.php
@@ -9,3 +9,18 @@ $aliases['local'] = array(
   'root' => '/var/www/federated-search-demo.local/web/',
   'uri' => 'www.federated-search-demo.local',
 );
+
+$aliases['local.ds_one'] = array(
+  'root' => '/var/www/federated-search-demo.local/web/',
+  'uri' => 'www.federated-search-demo.d8-1.local',
+);
+
+$aliases['local.ds_two'] = array(
+  'root' => '/var/www/federated-search-demo.local/web/',
+  'uri' => 'www.federated-search-demo.d8-2.local',
+);
+
+$aliases['local.ds_three'] = array(
+  'root' => '/var/www/federated-search-demo.local/web/',
+  'uri' => 'www.federated-search-demo.d8-3.local',
+);
