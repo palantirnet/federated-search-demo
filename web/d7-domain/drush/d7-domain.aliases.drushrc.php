d4558cf4df5567f16ecba00789e38703473defb2
Bec White <white@palantir.net>
2020-01-17 15:59:19 -0600

    Add drush directly to the project (Drush 10 for Drupal 8 and beyond, Drush 8 for Drupal 7)

--- web/d7-domain/drush/d7-domain.aliases.drushrc.php
+++ web/d7-domain/drush/d7-domain.aliases.drushrc.php
@@ -5,22 +5,17 @@ if (!isset($drush_major_version)) {
   $drush_major_version = $drush_version_components[0];
 }
 
-$aliases['local'] = array(
-  'root' => '/var/www/federated-search-demo.local/web/d7/docroot',
-  'uri' => 'd7.fs-demo.local',
-);
-
-$aliases['local.ds_one'] = array(
+$aliases['one'] = array(
     'root' => '/var/www/federated-search-demo.local/web/d7-domain/docroot',
     'uri' => 'd7-1.fs-demo.local',
 );
 
-$aliases['local.ds_two'] = array(
+$aliases['two'] = array(
     'root' => '/var/www/federated-search-demo.local/web/d7-domain/docroot',
     'uri' => 'd7-2.fs-demo.local',
 );
 
-$aliases['local.ds_three'] = array(
+$aliases['three'] = array(
     'root' => '/var/www/federated-search-demo.local/web/d7-domain/docroot',
     'uri' => 'd7-3.fs-demo.local',
 );
